4e76738c34c55903e86c611a61b962bfa4d2c879
EC2 Default User <ec2-user@ip-172-31-28-229.eu-north-1.compute.internal>
2026-06-20 10:19:35 +0000

    Terraform AWS EC2 Deployment Project

---
+++ terraform-sg/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Security Group for DevOps Server"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Nikhil-SG"
+  }
+}
